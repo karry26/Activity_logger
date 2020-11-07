2c4953c03caa55ac79eee868b35538df4c62a203
Kairav <kairav.bansal>
2020-11-08 00:33:02 +0530

    initial files and sql

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+	
+	$dbRef=mysqli_connect("localhost","root","","activity_logger");
+	
+	$msg=mysqli_connect_error();
+	if($msg=="")
+		echo "Connnnnected....,,.,.,.,";
+	else
+		echo $msg;
+	
+	/*Importing:
+
+mysql -u databaseuser -p -h localhost –-default-character-set=utf8 databasename < sqlfile.sql
+
+Dumpping:
+
+mysqldump -u databaseuser -p databasename --default-character-set=utf8 > sqlfile.sql
+mysqldump < mysqldump options> | gzip > outputfile.sql.gz*/
+?>
