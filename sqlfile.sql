0c528bf2ac9072ecca3bd27f4c82949a062b5f21
Umesh <44177472+23umesh@users.noreply.github.com>
2020-11-22 23:51:12 +0530

    added sql file

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,19 +1,52 @@
--- MySQL dump 10.17  Distrib 10.3.16-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.18  Distrib 10.4.16-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: activity_logger
 -- ------------------------------------------------------
--- Server version	10.3.16-MariaDB
+-- Server version	10.4.16-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `activity_logger`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `activity_logger` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `activity_logger`;
+
+--
+-- Table structure for table `meetings`
+--
+
+DROP TABLE IF EXISTS `meetings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `meetings` (
+  `topic` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `admin` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `meetings`
+--
+
+LOCK TABLES `meetings` WRITE;
+/*!40000 ALTER TABLE `meetings` DISABLE KEYS */;
+INSERT INTO `meetings` VALUES ('dfgdg','2020-11-21','17:23:00',''),('dfgdg','2020-11-21','17:23:00',''),('dfgdg','2020-11-21','17:23:00',''),('dfgdg','2020-11-21','17:23:00',''),('dfgdg','2020-11-21','17:23:00',''),('dfgdg','2020-11-07','22:29:00',''),('sgfgdfg','2020-11-14','00:29:00',''),('sgfgdfg','2020-11-14','00:29:00',''),('sgfgdfg','2020-11-14','00:29:00','');
+/*!40000 ALTER TABLE `meetings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -36,7 +69,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('Kairav Bansal','kairavbansla@gmail.com','9988154700','kairav');
+INSERT INTO `users` VALUES ('Kairav Bansal','kairavbansla@gmail.com','9988154700','kairav'),('','','umesh','13ceb369053f26c592bb4ae838714c33');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-08  0:26:11
+-- Dump completed on 2020-11-22 23:50:19
