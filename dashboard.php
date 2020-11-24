2ab123641142a38dcb19de2d3078cec38411bb76
Kairav <kairavbansal@gmail.com>
2020-11-24 20:45:21 +0530

    index page

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,10 @@
-
+<?php
+session_start();
+if(!isset($_SESSION["uid"]))
+{
+header("location:index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- index.php
+++ index.php
@@ -39,30 +39,24 @@ header("location:dashboard.php");
         {
         $('.login-info-box').fadeOut();
         $('.login-show').addClass('show-log-panel');
-        });
-
-
-        $('.active-log-panel input[type="radio"]').on('change', function() 
-        {
-            if($('#log-login-show').is(':checked')) {
-                $('.register-info-box').fadeOut(); 
+        $('#register-now').click(function(){
+            $('.register-info-box').fadeOut(); 
                 $('.login-info-box').fadeIn();
                 
                 $('.white-panel').addClass('right-log');
                 $('.register-show').addClass('show-log-panel');
                 $('.login-show').removeClass('show-log-panel');
-                
-            }
-            else if($('#log-reg-show').is(':checked')) {
-                $('.register-info-box').fadeIn();
+        })
+         $('#login-now').click(function(){
+             $('.register-info-box').fadeIn();
                 $('.login-info-box').fadeOut();
                 
                 $('.white-panel').removeClass('right-log');
                 
                 $('.login-show').addClass('show-log-panel');
                 $('.register-show').removeClass('show-log-panel');
-            }
-        });
+         })
+       
   
 
 
@@ -70,7 +64,7 @@ header("location:dashboard.php");
             {
                 var uid = $("#uidd").val();
                 var pwd = $("#pwdd").val();
- 
+               // alert(uid);
                 $.get("project-login.php?uid="+uid+ "&pwd="+pwd, function(response)
                 {
                     // alert('2');
@@ -83,85 +77,47 @@ header("location:dashboard.php");
             
             });
 
+        });
+
+
+       
+
     </script>
 </head>
 <body>
 
 
-
-<hr>
-
-
-    <!--div class=" container">
-    	<h1> Activity Logger</h1>
-        <div class="col-md-12">
-            <nav class="navbar navbar-dark bg-dark  navbar-expand-lg ">
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item ">
-                            <input type="button" class="btn btn-outline-success my-2 my-sm-0 nav-tabs nav-justified text-white" value="Sign Up" data-target="#exampleModalCenter" data-toggle="modal">
-                        </li>
-
-                        <li>
-                            <input type="button" class="btn btn-outline-success my-2 my-sm-0 text-white" value="Sign In" data-target="#exampleModalCenter2" data-toggle="modal">
-
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div-->
-
-    
-
     <div class="white-panel">
-        <div class="login-show">
-            <div class="container-login100" style="background-image: url('pics/bg.jpg');">
+        <div class="login-show" style="background-image: url('pics/bg.jpg');">
+            <div class="container-login100" >
                 <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-                    <form class="login100-form validate-form">
+                    <form class="login100-form validate-form" >
                         <span class="login100-form-title p-b-49">
                             Login
                         </span>
 
                         <div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
                             <span class="label-input100">Username</span>
-                            <input class="input100" type="text" name="username" placeholder="Type your username">
+                            <input class="input100" type="text" id="uidd" name="username" placeholder="Type your username">
                             <span class="focus-input100" data-symbol="&#xf206;"></span>
                         </div>
 
                         <div class="wrap-input100 validate-input" data-validate="Password is required">
                             <span class="label-input100">Password</span>
-                            <input class="input100" type="password" name="pass" placeholder="Type your password">
+                            <input class="input100" type="password" id="pwdd" name="pass" placeholder="Type your password">
                             <span class="focus-input100" data-symbol="&#xf190;"></span>
                         </div>
-                        
+                        <br>
                         <div class="container-login100-form-btn">
                             <div class="wrap-login100-form-btn">
                                 <div class="login100-form-bgbtn"></div>
-                                <button class="login100-form-btn">
+                                <button class="login100-form-btn" id="signup">
                                     Login
                                 </button>
                             </div>
                         </div>
 
-                        <!---div class="login-info-box">
-                            <div class="txt1 text-center p-t-54 p-b-20">
-                                <span>
-                                    Don't have an account?
-                                </span>
-                            </div>
-
-                            <div class="flex-c-m">
-                                <a href="#" class="txt2">
-                                    Sign Up
-                                </a>
-                            </div>
-                        </div--->
+                       
 
                     </form>
                 </div>
@@ -169,17 +125,17 @@ header("location:dashboard.php");
         </div>
 
 		
-        <div class="register-show">
-            <div class="container-login100" style="background-image: url('pics/bg.jpg');">
+        <div class="register-show" style="background-image: url('pics/bg.jpg');" >
+            <div class="container-login100">
                 <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-                    <form class="login100-form validate-form">
+                    <form class="login100-form validate-form" action=project-signup.php method="post" enctype="multipart/form-data">
                         <span class="login100-form-title p-b-49">
                             Sign Up
                         </span>
 
-                        <div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+                        <div class="wrap-input100 validate-input m-b-23" data-validate = "Username is required">
                             <span class="label-input100">Username</span>
-                            <input class="input100" type="text" name="username" placeholder="Type your username">
+                            <input class="input100" type="text" name="uid" placeholder="Type your username">
                             <span class="focus-input100" data-symbol="&#xf206;"></span>
                         </div>
 
@@ -191,9 +147,9 @@ header("location:dashboard.php");
                         
                         <div class="wrap-input100 validate-input" data-validate="Password is required">
                             <span class="label-input100">Password</span>
-                            <input class="input100" type="password" name="pass" placeholder="Type your password">
+                            <input class="input100" type="password" name="pwd" placeholder="Type your password">
                             <span class="focus-input100" data-symbol="&#xf190;"></span>
-                        </div>
+                        </div><br>
                         
                         <div class="container-login100-form-btn">
                             <div class="wrap-login100-form-btn">
@@ -204,21 +160,7 @@ header("location:dashboard.php");
                             </div>
                         </div>
 
-                        <!---div class="login-info-box">
-                            <div class="txt1 text-center p-t-54 p-b-20">
-                                <span>
-                                    Already have an account?
-                                </span>
-                            </div>
-
-                            <div class="flex-c-m">
-                                <a href="#" class="txt2">
-                                    Login
-                                </a>
-                                <label id="label-register" for="log-reg-show">Login</label>
-                                <input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked">
-                            </div>
-                        </div--->
+                        
 
                     </form>
                 </div>
@@ -229,91 +171,15 @@ header("location:dashboard.php");
     <div class="login-reg-panel">
 
         <div class="login-info-box">
-            <h2>Have an account?</h2>
-            <label id="label-register" for="log-reg-show">Login</label>
-            <input type="radio" name="active-log-panel" id="log-reg-show" checked="checked">
+            <br>
+            <h5>Have an account?</h5><button type="button" class="btn btn-primary btn-sm" id="login-now">Login Now</button>
+           
         </div>
                             
         <div class="register-info-box">
-            <h2>Don't have an account?</h2>
-            
-            <label id="label-login" for="log-login-show">Register</label>
-            <input type="radio" name="active-log-panel" id="log-login-show">
+            <h5>Don't have an account?</h5>
+             <button type="button" class="btn btn-primary btn-sm" id="register-now">Register Now</button>
+           
         </div>
     </div>
 
-
-    <!-- Modal -->
-    <form action=project-signup.php method="post" enctype="multipart/form-data">
-       
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">SIGN UP</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-row">
-                            <div class="col-md-12 form-group">
-                                <label for="inputEmail4">Name</label>
-                                <input type="text" class="form-control" id="name" placeholder="Name" name="name">
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <label for="inputEmail4">Username</label>
-                                <input type="text" class="form-control" id="uid" placeholder="Username" name="uid">
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <label for="inputEmail4">Password</label>
-                                <input type="password" class="form-control " id="pwd" name="pwd" placeholder="Password" p>
-                            </div>
-
-
-                          
-
-                        </div>
-                    </div>
-                    <div class="modal-footer align-items-center">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" name='btn' value="Save">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-    </form>
-    <form action="project-login.php" 
-				method="get" enctype="multipart/form-data">
-    <div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">SIGN IN</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-row">
-                        <div class="col-md-12 form-group">
-                            <label for="uidd">Username</label>
-                            <input type="text" class="form-control" id="uidd" placeholder="Username" name="uidd">
-                        </div>
-                        <div class="col-md-12 form-group">
-                            <label for="inputEmail4">Password</label>
-                            <input type="password" class="form-control " id="pwdd" placeholder="Password" name=pwdd>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer align-items-center">
-                    <button  class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button   type = "button" id="sign" class="btn btn-primary">Sign IN</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    </form>
